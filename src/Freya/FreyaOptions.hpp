d7476bcb55c0ddf4c46ba51d32f4fd257191aad1
gilmar-sales <gilmar.custodio@outlook.com.br>
2025-04-07 01:16:43 -0300

    Add rendering strategy to freya options

--- src/Freya/FreyaOptions.hpp
+++ src/Freya/FreyaOptions.hpp
@@ -2,17 +2,24 @@
 
 namespace FREYA_NAMESPACE
 {
+    enum class RenderingStrategy
+    {
+        Forward,
+        Deferred,
+    };
+
     struct FreyaOptions
     {
-        std::string         title        = "Freya Window";
-        std::uint32_t       width        = 800;
-        std::uint32_t       height       = 600;
-        bool                vSync        = true;
-        bool                fullscreen   = true;
-        std::uint32_t       sampleCount  = 1;
-        std::uint32_t       frameCount   = 4;
-        vk::ClearColorValue clearColor   = { 0.0f, 0.0f, 0.0f, 0.0f };
-        float               drawDistance = 1000.0f;
+        std::string         title             = "Freya Window";
+        std::uint32_t       width             = 800;
+        std::uint32_t       height            = 600;
+        bool                vSync             = true;
+        bool                fullscreen        = true;
+        std::uint32_t       sampleCount       = 1;
+        std::uint32_t       frameCount        = 4;
+        vk::ClearColorValue clearColor        = { 0.0f, 0.0f, 0.0f, 0.0f };
+        float               drawDistance      = 1000.0f;
+        RenderingStrategy   renderingStrategy = RenderingStrategy::Forward;
     };
 
 } // namespace FREYA_NAMESPACE
